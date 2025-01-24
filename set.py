3995a3eaa50c224951fb10a56a2e64bcf16218cc
gowsalya-98 <gowsalyasrinu@gmail.com>
2025-01-24 10:21:26 +0530

    changes

---
+++ set.py
@@ -0,0 +1,71 @@
+# add method
+# book={"tamilbook","Englishbook","mathsboo","sciencebook","socialbook"}
+# book.add("gkbook")
+# print(book)
+# clear method
+# book={"tamilbook","Englishbook","mathsboo","sciencebook","socialbook"}
+# book.clear()
+# print(book)
+# copy method
+# book={"tamilbook","Englishbook","mathsboo","sciencebook","socialbook"}
+# book.copy()
+# print(book)
+# difference(-) method
+# book1={"tamilbook","Englishbook","mathsboo","sciencebook","socialbook"}
+# book2={"tamilbook","Englishbook","gkbook"}
+# newbook=book1.difference(book2)
+# print(newbook)
+# difference_update(-=)method
+# book1={"tamilbook","Englishbook","mathsboo","sciencebook","socialbook"}
+# book2={"tamilbook","Englishbook","gkbook"}
+# book1.difference_update(book2)
+# print(book1)
+# discard method
+# book1={"tamilbook","Englishbook","mathsboo","sciencebook","socialbook"}
+# book1.discard("tamilbook",)
+# print(book1)
+# intersection(&)method
+# book1={"tamilbook","Englishbook","mathsboo","sciencebook","socialbook"}
+# book2={"tamilbook","Englishbook","gkbook"}
+# newbook=book1.intersection(book2)
+# print(newbook)
+# intersection_update(&=)method
+# book1={"tamilbook","Englishbook","mathsboo","sciencebook","socialbook"}
+# book2={"tamilbook","Englishbook","gkbook"}
+# book1.intersection_update(book2)
+# print(book1)
+# disjoint method
+# book1={"tamilbook","Englishbook","mathsboo","sciencebook","socialbook"}
+# book2={"tamilbook","Englishbook","gkbook"}
+# newbook=book1.isdisjoint(book2)
+# print(newbook)
+# issubset(<=) method
+# book1={"tamilbook","Englishbook","mathsboo","sciencebook","socialbook"}
+# book2={"tamilbook","Englishbook","gkbook"}
+# newbook=book1.issubset(book2)
+# issuperset(>=) method
+# book1={"tamilbook","Englishbook","mathsboo","sciencebook","socialbook"}
+# book2={"tamilbook","Englishbook","gkbook"}
+# newbook=book1.issuperset(book2)
+# print(newbook)
+# pop method
+# book2={"tamilbook","Englishbook","gkbook"}
+# book2.pop()
+# print(book2)
+# remove method
+# book2={"tamilbook","Englishbook","gkbook"}
+# book2.remove("tamilbook")
+# print(book2)
+# symmertric_difference(^)method
+# book1={"tamilbook","Englishbook","mathsboo","sciencebook","socialbook"}
+# book2={"tamilbook","Englishbook","gkbook"}
+# newbook=book1.symmetric_difference(book2)
+# union method(|)
+# book1={"tamilbook","Englishbook","mathsboo","sciencebook","socialbook"}
+# book2={"tamilbook","Englishbook","gkbook"}
+# newbook=book1.union(book2)
+# print(newbook)
+# update(|=) method
+# book1={"tamilbook","Englishbook","mathsboo","sciencebook","socialbook"}
+# book2={"tamilbook","Englishbook","gkbook"}
+# book1.update(book2)
