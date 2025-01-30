93cf237834a22d8f1b3c702d5655ccb1ab310df1
gowsalya-98 <gowsalyasrinu@gmail.com>
2025-01-30 12:03:15 +0530

    changes

--- LAMBDA FUNCTION  TASK.py
+++ LAMBDA FUNCTION  TASK.py
@@ -2,7 +2,6 @@ x=lambda a,b:a+b
 print(x(10,35))
 
 
-
 x=lambda a:a*a
 print(x(10))
 
@@ -14,4 +13,5 @@ print(x("hello"))
 x=lambda a:a%2==0
 print(x(13))
 
-x
+x=lambda a,b: a if a>b else b
+print(x(10,20))
---
+++ RECURSION TASK.py
@@ -0,0 +1,43 @@
+# def number(n=1):
+#     if n>10:
+#         return
+#     print(n)
+#     number(n+1)
+# number()
+
+
+
+# def number(n=10):
+#     if n<1:
+#         return
+#     print(n)
+#     number(n-1)
+# number()
+
+# def fibonacci(n):
+#     if n<=0:
+#         return 
+#     elif n==1:
+#         return 0
+#     elif n==2:
+#         return 1
+#     else:
+#         return fibonacci(n-1)+fibonacci(n-2)
+
+# def series(n):
+#     for i in range(1,n+1):
+#         print(fibonacci(i),end="")
+# numberseries=10
+# series(numberseries)
+
+# def sum(l):
+#    sum=0 
+#    for num in l:
+#         sum+=num
+#         print(sum)
+#    print("the sum", sum)
+# l=[1,2,3,4,5]
+# sum(l)
+
+
+        
