725d081705b0fada5211358f527a68765bac928b
gowsalya-98 <gowsalyasrinu@gmail.com>
2025-02-04 10:11:15 +0530

    changes

--- modules.py
+++ modules.py
@@ -1,7 +1,35 @@
-# import pymodules
-# pymodules.square(5)
+# # import pymodules
+# # pymodules.square(5)
 
-from pymodules import person2 
-print (person2["country"])
+# # from pymodules import person1 
+# # print (person1["age"])
+
+# # math moduls
+
+# # import math
+# # result=math.factorial(5)
+# # print(result)
+
+# # import math
+# # result=math.sqrt(3)
+# # print(result)
+
+
+# # import math
+# # cube=math.pow(3,3)
+# # print(cube)
+
+
+# import math
+
+
+import random
+my_list=["apple","red","time"]
+a=random.choice(my_list)
+print(a)
+
+
+
+
 
 
--- pymodules.py
+++ pymodules.py
@@ -3,16 +3,16 @@
 # print("the area of square",square(6))
 
 
-def school(name):
-  print("Hello, " + name)
+# def school(name):
+#   print("Hello, " + name)
 
-person1 = {
-  "name": "gowsalya",
-  "age": 36,
-  "country": "india"
-}
-person2={
-  "name":"sudha",
-  "age":34,
-  "country":"usa"
-}
+# person1 = {
+#   "name": "gowsalya",
+#   "age": 36,
+#   "country": "india"
+# }
+# person2={
+#   "name":"sudha",
+#   "age":34,
+#   "country":"usa"
+# }
