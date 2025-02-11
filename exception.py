8c17d810e72591cf38976ed30cf2c224fdd1accf
gowsalya-98 <gowsalyasrinu@gmail.com>
2025-02-11 10:38:46 +0530

    change

---
+++ exception.py
@@ -0,0 +1,52 @@
+1
+number= int(input("enter the number:"))
+try: 
+    result=1/number
+except ZeroDivisionError:
+    print("can't work this operation")
+finally:
+    print("process completed")
+2
+number= int(input("enter the number:"))
+try: 
+    result=1/number
+except ZeroDivisionError:
+    print("can't work this operation")
+except ValueError:
+    print("enter the value number:")
+finally:
+    print("process completed")
+3
+
+number= int(input("enter the number:"))
+try: 
+    square= number**2
+except ZeroDivisionError:
+    print("can't work this operation")
+else: 
+    print("square value:",square)
+finally:
+    print("process completed")
+
+4
+number= int(input("enter the number:"))
+try: 
+    square= number**2
+except ZeroDivisionError:
+    print("can't work this operation")
+except ValueError:
+    print("invalid number")
+else: 
+    print("square value:",square)
+finally:
+    print("process completed")
+
+5
+try: 
+    n=int(input("enter the value"))
+    if(n<0):
+        print("negative number",n)      
+except ValueError:
+    print("invalid number")
+finally:
+    print("process completed")
