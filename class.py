0f60d9372970b37c88ba4fa2d8d07e0006e89f1a
gowsalya-98 <gowsalyasrinu@gmail.com>
2025-02-18 12:18:41 +0530

    changes

---
+++ class.py
@@ -0,0 +1,31 @@
+class sbi_bank():
+    credit=2000
+    debit=4000
+#     loan=10000
+# sbi_bank1=sbi_bank()
+# print(sbi_bank1.debit)
+# a=sbi_bank1.debit=1500
+# print(a)
+# sbi_bank2=sbi_bank()
+# print(sbi_bank2.loan)
+
+    def cash(self):
+        print(" More amount transaction of sbi bank 1")
+    def loan1(self):
+        print("less transaction in loan devision")
+    def credit1(self):
+        print("people not ready to tranction" )
+bank1=sbi_bank()
+bank1.cash()
+bank2=sbi_bank()
+bank2.loan1()
+bank3=sbi_bank()
+bank3.credit1()
+
+
+# a=sbi_bank.credit1=("more tracsaction")
+# print(a)
+
+a=sbi_bank()
+b=a.credit1=("moe")
+print(b)
--- exception.py
+++ exception.py
@@ -1,7 +1,8 @@
 1
 number= int(input("enter the number:"))
 try: 
-    result=1/number
+    a=1/number
+    print(a)
 except ZeroDivisionError:
     print("can't work this operation")
 finally:
@@ -10,6 +11,7 @@ finally:
 number= int(input("enter the number:"))
 try: 
     result=1/number
+    print(result)
 except ZeroDivisionError:
     print("can't work this operation")
 except ValueError:
@@ -21,6 +23,7 @@ finally:
 number= int(input("enter the number:"))
 try: 
     square= number**2
+    print(square)
 except ZeroDivisionError:
     print("can't work this operation")
 else: 
@@ -32,6 +35,7 @@ finally:
 number= int(input("enter the number:"))
 try: 
     square= number**2
+    print(square)
 except ZeroDivisionError:
     print("can't work this operation")
 except ValueError:
