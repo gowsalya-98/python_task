66cba7c913fc1f6a9adc951856f1c6cf7f614fc6
gowsalya-98 <gowsalyasrinu@gmail.com>
2025-02-20 12:08:00 +0530

    changes

--- class.py
+++ class.py
@@ -1,31 +1,41 @@
-class sbi_bank():
-    credit=2000
-    debit=4000
-#     loan=10000
-# sbi_bank1=sbi_bank()
-# print(sbi_bank1.debit)
-# a=sbi_bank1.debit=1500
-# print(a)
-# sbi_bank2=sbi_bank()
-# print(sbi_bank2.loan)
+# class sbi_bank():
+#     credit=2000
+#     debit=4000
+# #     loan=10000
+# # sbi_bank1=sbi_bank()
+# # print(sbi_bank1.debit)
+# # a=sbi_bank1.debit=1500
+# # print(a)
+# # sbi_bank2=sbi_bank()
+# # print(sbi_bank2.loan)
 
-    def cash(self):
-        print(" More amount transaction of sbi bank 1")
-    def loan1(self):
-        print("less transaction in loan devision")
-    def credit1(self):
-        print("people not ready to tranction" )
-bank1=sbi_bank()
-bank1.cash()
-bank2=sbi_bank()
-bank2.loan1()
-bank3=sbi_bank()
-bank3.credit1()
+#     def cash(self):
+#         print(" More amount transaction of sbi bank 1")
+#     def loan1(self):
+#         print("less transaction in loan devision")
+#     def credit1(self):
+#         print("people not ready to tranction" )
+# bank1=sbi_bank()
+# bank1.cash()
+# bank2=sbi_bank()
+# bank2.loan1()
+# bank3=sbi_bank()
+# bank3.credit1()
 
 
-# a=sbi_bank.credit1=("more tracsaction")
-# print(a)
+# # a=sbi_bank.credit1=("more tracsaction")
+# # print(a)
 
-a=sbi_bank()
-b=a.credit1=("moe")
-print(b)
+# a=sbi_bank()
+# b=a.credit1=("moe")
+# print(b)
+
+
+class school:
+   def __init__(self ,1_class,2_class,3_class,4_class):
+    self.1_class=1_class 
+    self.2_class=2_class
+    self.3_class=3_class
+    self.4_class=4_class
+   def 1_class(self):
+     
---
+++ inheritance.py
@@ -0,0 +1,90 @@
+    #   (1.single methods)
+class father:
+    def father_methods(self):
+        print("father method useing")
+class baby(father):
+    def baby_methods(self):
+        print("father methods useing baby methods")
+obj=baby()
+obj.father_methods()
+obj.baby_methods()
+
+#         (2.multiple inheritance)
+class father:
+    def methods1(self):
+        print("father methods used")
+class mother:
+    def methods2(self):
+        print("mother methods useing")
+class baby(father,mother):
+    print ("both methods useing")
+obj=baby()
+obj.methods1()
+obj.methods2()
+
+#         (3.multilevel inheritances)
+class thatha():
+    def methods1(self):
+        print("thatha methods is very usefull")
+class father(thatha):
+    def methods2(self):
+        print("father methods used thatha methods ")
+class baby(father):
+    def methods3(self):
+        print("both methods are very usefull")
+obj=baby()
+obj.methods3()
+obj.methods2()
+obj.methods1()
+
+#       (4.hirarchical inheritance)
+class father():
+    def methods1(self):
+        print("father methods useing babys ")
+class baby1(father):
+      pass
+class baby2(father):
+     pass
+obj=baby2()
+obj=baby1()
+obj.methods1()
+
+#     (5.hybrid inheritances)
+class number1():
+    def methods1(self):
+        print("useing number 1")
+class number2(number1):
+    def methods2(self):
+        print("number2 useing number1 methods ")
+class number3(number1):
+    def methods3(self):
+        print("number3 useing number1 methods")
+class number4(number2,number3):
+    def methods4(self):
+        print("number4 useing number2 and number3 ")
+obj=number4()
+obj.methods4()
+obj.methods1()
+obj.methods2()
+obj.methods3()
+    
+#           (6.overring in inheritances)
+class father():
+    def method(self):
+        print("father method usefull")
+class baby(father):
+    def method(self):
+        print("father methods not usefull")
+obj=baby()
+obj.method()
+
+#            (7.super() methods)
+class father():
+    def method(self):
+        print("father is most usefull")
+class baby(father):
+    def method(self):
+        super().method()
+        print("father and baby methods also using")
+obj=baby()
+obj.method()
