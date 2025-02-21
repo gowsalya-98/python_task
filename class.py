2f9cba040c426355fc94277ffbdca12d5a073675
gowsalya-98 <gowsalyasrinu@gmail.com>
2025-02-21 11:05:39 +0530

    changes

--- class.py
+++ class.py
@@ -1,3 +1,5 @@
+# 
+# (1.cls and object)
 # class sbi_bank():
 #     credit=2000
 #     debit=4000
@@ -30,12 +32,26 @@
 # b=a.credit1=("moe")
 # print(b)
 
+#  (2.cls and object with constructor)
 
-class school:
-   def __init__(self ,1_class,2_class,3_class,4_class):
-    self.1_class=1_class 
-    self.2_class=2_class
-    self.3_class=3_class
-    self.4_class=4_class
-   def 1_class(self):
+class phone:
+   def __init__(self ,a_call,b_call,c_call,d_call):
+      self.a_call=a_call
+      self.b_call=b_call
+      self.c_call=c_call
+      self.d_call=d_call
+   def a(self):
+      print(f"a_call is messages you {self.a_call}")
+   def b(self):
+      print(f"b_call is whatsapp msg you {self.b_call}")
+   def c(self):
+      print(f"look a instragram pages {self.c_call}")
+   def d(self):
+      print(f"using more times in mobile {self.d_call}")
+
+obj=phone("ring1","ring2","ring3","ring4")
+obj.a()
+obj.b()
+obj.c()
+obj.d()  
      
